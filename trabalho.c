46a921f25f360926208340a9d3a9e3b66a0fe943
brunocosta1 <brunocosta.cg1998@gmail.com>
2021-11-30 22:56:30 -0300

    Iniciando o trabalho

---
+++ trabalho.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct Palavra{
+    char letras[50]; // Caracteres que formam a palavra.
+    int qtdOcorrencias; // Quantidade de ocorrências no arquivo.
+    int *linhas; // Vetor dinâmico.
+};
+
+
+
+int main(){
+
+
+    return 0;
+}
